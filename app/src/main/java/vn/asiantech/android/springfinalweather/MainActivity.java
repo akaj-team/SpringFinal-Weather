b8237e7214b778bec518b2096c0a90a21f3c11a7
Tai Nguyen D <tai.nguyen@asiantech.vn>
2018-05-18 18:35:21 +0700

    test fix CI's comment

--- app/src/main/java/vn/asiantech/android/springfinalweather/MainActivity.java
+++ app/src/main/java/vn/asiantech/android/springfinalweather/MainActivity.java
@@ -4,7 +4,6 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
-    private String TEST_A;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
