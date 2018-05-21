9742cfe29e4a132f5963ea70325c458e024ecc0e
at-huynguyen3 <huy.nguyen3@asiantech.vn>
2018-05-21 13:53:56 +0700

    create recycler view

--- app/src/main/java/vn/asiantech/android/springfinalweather/MainActivity.java
+++ app/src/main/java/vn/asiantech/android/springfinalweather/MainActivity.java
@@ -7,6 +7,7 @@ import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
+import android.support.v4.view.ViewPager;
 
 import java.util.Objects;
 
@@ -18,6 +19,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ViewPager viewPager = findViewById(R.id.viewPager);
         initViews();
         initListener();
     }
