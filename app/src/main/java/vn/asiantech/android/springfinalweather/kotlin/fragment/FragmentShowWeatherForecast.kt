e4f6587d3141e04cf9ad851a559ae94a0a209196
at-huynguyen3 <huy.nguyen3@asiantech.vn>
2018-05-24 14:54:10 +0700

    create wind speed

--- app/src/main/java/vn/asiantech/android/springfinalweather/kotlin/fragment/FragmentShowWeatherForecast.kt
+++ app/src/main/java/vn/asiantech/android/springfinalweather/kotlin/fragment/FragmentShowWeatherForecast.kt
@@ -1,9 +1,12 @@
 package vn.asiantech.android.springfinalweather.kotlin.fragment
 
 import android.annotation.SuppressLint
+import android.content.Context
+import android.content.SharedPreferences
 import android.support.v4.app.Fragment
 import android.os.Bundle
 import android.text.TextUtils
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -15,10 +18,12 @@ import vn.asiantech.android.springfinalweather.R
 import vn.asiantech.android.springfinalweather.kotlin.`object`.Constants
 import vn.asiantech.android.springfinalweather.kotlin.apiservice.ApiServices
 import vn.asiantech.android.springfinalweather.kotlin.model.InformationtWeather
+import java.math.BigDecimal
 import java.text.SimpleDateFormat
 import java.util.*
 
 class FragmentShowWeatherForecast : Fragment() {
+    private var mSharedPreferences: SharedPreferences? = null
     private lateinit var mTvCurrentDay: TextView
     private lateinit var mTvCountryName: TextView
     private lateinit var mEdtSearch: EditText
@@ -33,6 +38,7 @@ class FragmentShowWeatherForecast : Fragment() {
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         val view = inflater.inflate(R.layout.fragment_show_weather_forecast, container, false)
         initViews(view)
+        Log.d("zxc", "onCreat: ")
         setListener()
         return view
     }
@@ -73,6 +79,20 @@ class FragmentShowWeatherForecast : Fragment() {
         val cloudsBean = informationtWeather.clouds
         val windBean = informationtWeather.wind
 
+        mSharedPreferences = activity?.getPreferences(Context.MODE_PRIVATE)
+        Log.d("zxv", mSharedPreferences?.getInt(Constants.UNIT_OF_TEMP, 0).toString())
+        if (mSharedPreferences?.getInt(Constants.UNIT_OF_TEMP, 0) == 0) {
+            mTvWind.text = getKilomet(windBean?.speed?.toFloat()).toString() + " km/h"
+        } else {
+            mTvWind.text = windBean?.speed.toString() + " m/s"
+        }
+
+        if (mSharedPreferences?.getInt(Constants.UNIT_OF_TEMP, 0) == 0) {
+            mTvTemp.text = getCelsiusDegree(mainBean?.temp?.toFloat()).toString() + "°C"
+        } else {
+            mTvTemp.text = getFahrenheit(mainBean?.temp?.toFloat()).toString() + "°F"
+        }
+
         @SuppressLint("SimpleDateFormat")
         val simpleDateFormat = SimpleDateFormat("EEEE dd-MM-yyyy")
         val day = informationtWeather.dt
@@ -81,13 +101,29 @@ class FragmentShowWeatherForecast : Fragment() {
         val time = simpleDateFormat.format(date)
         mTvCurrentDay.text = time
         mTvCountryName.text = informationtWeather.name + ", " + sysBean?.country
-        mTvTemp.text = mainBean?.temp.toString() + "°K"
         val icon = weatherBean?.get(0)?.icon
         mImgIcon.setImageResource(getIcon(icon.toString()))
         mTvStatus.text = weatherBean?.get(0)?.main
-        mTvHumidity.text = mainBean?.humidity.toString()
+        mTvHumidity.text = mainBean?.humidity.toString() + "%"
         mTvCloud.text = cloudsBean?.all.toString() + "%"
-        mTvWind.text = windBean?.speed.toString() + " m/s"
+    }
+
+    private fun getKilomet(speed: Float?): Float? {
+        val convert = BigDecimal("3.6")
+        val kilometer = convert.setScale(2, BigDecimal.ROUND_HALF_EVEN).toFloat()
+        return (speed?.times(kilometer))
+    }
+
+    private fun getCelsiusDegree(cel: Float?): Float? {
+        val convert = BigDecimal("273.15")
+        val celsius = convert.setScale(2, BigDecimal.ROUND_HALF_EVEN).toFloat()
+        return (cel?.minus(celsius))
+    }
+
+    private fun getFahrenheit(fah: Float?): Float? {
+        val convert = BigDecimal("2.05")
+        val fahrenheit = convert.setScale(2, BigDecimal.ROUND_HALF_EVEN).toFloat()
+        return (fah?.div(fahrenheit))
     }
 
     private fun getIcon(icon: String): Int {
@@ -117,7 +153,7 @@ class FragmentShowWeatherForecast : Fragment() {
     private fun setListener() {
         mBtnSearch.setOnClickListener { view ->
             if (view.id == R.id.btnSearch) {
-                val searchCity = mEdtSearch.text.toString().trim { it <= ' ' }
+                val searchCity = mEdtSearch.text.toString().trim()
                 if (!TextUtils.isEmpty(searchCity)) {
                     loadInformationWeather(searchCity)
                 }
