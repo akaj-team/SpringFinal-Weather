8a573eed66ae36284ca3e00d7a06068f1fa9eabe
duc.nguyen2 <duc.nguyen2@asiantech.vn>
2018-06-01 09:36:45 +0700

    [Update] New Icon

--- app/src/main/java/vn/asiantech/android/springfinalweather/kotlin/object/Constants.kt
+++ app/src/main/java/vn/asiantech/android/springfinalweather/kotlin/object/Constants.kt
@@ -8,11 +8,6 @@ class Constants {
         const val UNIT_OF_WIND_SPEED = "unit_of_wind_speed"
         const val LOCATION_PERMISSION_REQUEST = 1
 
-        // const for Google find City Api
-        const val BASE_URL_API_GOOGLE = "https://maps.googleapis.com"
-        const val CITY_TYPES = "(cities)"
-        const val GOOGLE_KEY = "AIzaSyAvt30-GCmebRk-RB4QOlljVrCnepnyMHA"
-
         //const for intent key
         const val CITY_NAME = "city_name"
         const val DATE = "date"
@@ -28,20 +23,65 @@ class Constants {
         const val CANBACK = "can_back"
         const val FINDLOCATION = "find_location"
 
-        //const for OpenWeatherApi
+        //const for Api
         const val KEY = "b314dbc7647740fbbfe45806183105"
-        const val BASE_URL = "https://api.weatherbit.io"
-        const val DAYS = 5
-
-        //const for Xu api
         const val BASE_URL_WEATHER = "http://api.apixu.com/v1/"
-
-        //const for icon weather
+        const val DAYS = 5
 
         //const for State of City Collection
         const val USER_LOCATION = true
         const val OTHER_LOCATION = false
         const val FOCUS_POSITION = "focus_position"
         const val NAME_LOCATION = "name_location"
+
+        //const for icon, image weather
+        const val CODE_1000 = "1000"
+        const val CODE_1003 = "1003"
+        const val CODE_1006 = "1006"
+        const val CODE_1009 = "1009"
+        const val CODE_1030 = "1030"
+        const val CODE_1063 = "1063"
+        const val CODE_1066 = "1066"
+        const val CODE_1069 = "1069"
+        const val CODE_1072 = "1072"
+        const val CODE_1087 = "1087"
+        const val CODE_1114 = "1114"
+        const val CODE_1117 = "1117"
+        const val CODE_1135 = "1135"
+        const val CODE_1147 = "1147"
+        const val CODE_1150 = "1150"
+        const val CODE_1153 = "1153"
+        const val CODE_1168 = "1168"
+        const val CODE_1171 = "1171"
+        const val CODE_1180 = "1180"
+        const val CODE_1183 = "1183"
+        const val CODE_1186 = "1186"
+        const val CODE_1189 = "1189"
+        const val CODE_1192 = "1192"
+        const val CODE_1195 = "1195"
+        const val CODE_1198 = "1198"
+        const val CODE_1201 = "1201"
+        const val CODE_1204 = "1204"
+        const val CODE_1207 = "1207"
+        const val CODE_1210 = "1210"
+        const val CODE_1213 = "1213"
+        const val CODE_1216 = "1216"
+        const val CODE_1219 = "1219"
+        const val CODE_1222 = "1222"
+        const val CODE_1225 = "1225"
+        const val CODE_1237 = "1237"
+        const val CODE_1240 = "1240"
+        const val CODE_1243 = "1243"
+        const val CODE_1246 = "1246"
+        const val CODE_1249 = "1249"
+        const val CODE_1252 = "1252"
+        const val CODE_1255 = "1255"
+        const val CODE_1258 = "1258"
+        const val CODE_1261 = "1261"
+        const val CODE_1264 = "1264"
+        const val CODE_1273 = "1273"
+        const val CODE_1276 = "1276"
+        const val CODE_1279 = "1279"
+        const val CODE_1282 = "1282"
     }
 }
